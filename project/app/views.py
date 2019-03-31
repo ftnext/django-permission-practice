82725006a6abe60920a552dc78afb21d76ea0dbd
ftnext <takuyafjp+develop@gmail.com>
2019-03-31 16:38:56 +0900

    [feat] messageの一覧、追加、更新、削除用のビューの設定
    * reverse_lazyのimport元を変更してエラーに対処した

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import Message
 
-# Create your views here.
+
+class MesIndexView(generic.ListView):
+    model = Message
+
+
+class MesAddView(PermissionRequiredMixin, generic.CreateView):
+    model = Message
+    fields = "__all__"
+    success_url = reverse_lazy("app:index")
+    permission_required = ("app.add_message",)
+    raise_exception = True
+
+
+class MesChangeView(PermissionRequiredMixin, generic.UpdateView):
+    model = Message
+    fields = "__all__"
+    success_url = reverse_lazy("app:index")
+    permission_required = ("app.change_message",)
+    raise_exception = True
+
+
+class MesDeleteView(PermissionRequiredMixin, generic.DeleteView):
+    model = Message
+    fields = "__all__"
+    success_url = reverse_lazy("app:index")
+    permission_required = ("app.delete_message",)
+    raise_exception = True
